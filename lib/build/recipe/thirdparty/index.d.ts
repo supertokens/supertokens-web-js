ae7012c36dcab7d0e50da2e67b7608161377d9b1
Nemi Shah <nemishah1212@gmail.com>
2022-04-12 13:45:11 +0530

    Add code docs for thirdarty functions in recipe index files

--- lib/build/recipe/thirdparty/index.d.ts
+++ lib/build/recipe/thirdparty/index.d.ts
@@ -11,6 +11,23 @@ import {
 export default class RecipeWrapper {
     static init(config?: UserInput): import("../../types").CreateRecipeFunction<PreAndPostAPIHookAction>;
     static signOut(input?: { userContext?: any }): Promise<void>;
+    /**
+     * Get the URL that should be opened for third party authentication
+     *
+     * @param providerId The identifier for the third party provider. The value must match one of the providers configured with the backend SDK
+     *
+     * @param authorisationURL The URL that should be used for redirection after the third party flow finishes. This is ignored if the backend has a pre-configured redirect_url
+     *
+     * @param providerClientId (OPTIONAL) Client id to be used for the third party provider
+     *
+     * @param userContext (OPTIONAL) Refer to {@link https://supertokens.com/docs/thirdparty/advanced-customizations/user-context the documentation}
+     *
+     * @param options (OPTIONAL) Use this to configure additional properties (for example pre api hooks)
+     *
+     * @returns URL string
+     *
+     * @throws STGeneralError if the API exposed by the backend SDKs returns `status: "GENERAL_ERROR"`
+     */
     static getAuthorisationURLWithQueryParamsAndSetState(input: {
         providerId: string;
         authorisationURL: string;
@@ -18,6 +35,19 @@ export default class RecipeWrapper {
         userContext?: any;
         options?: RecipeFunctionOptions;
     }): Promise<string>;
+    /**
+     * Sign up/Sign in the user, this method uses the login attempt information from storage
+     *
+     * @param userContext (OPTIONAL) Refer to {@link https://supertokens.com/docs/thirdparty/advanced-customizations/user-context the documentation}
+     *
+     * @param options (OPTIONAL) Use this to configure additional properties (for example pre api hooks)
+     *
+     * @returns `{status: OK, user, createdNewUser: boolean}` if succesful
+     *
+     * @returns `{status: "NO_EMAIL_GIVEN_BY_PROVIDER"}` if the correct scopes are not configured for the third party provider
+     *
+     * @throws STGeneralError if the API exposed by the backend SDKs returns `status: "GENERAL_ERROR"`
+     */
     static signInAndUp(input?: { userContext?: any; options?: RecipeFunctionOptions }): Promise<
         | {
               status: "OK";
--- lib/ts/recipe/thirdparty/index.ts
+++ lib/ts/recipe/thirdparty/index.ts
@@ -37,6 +37,23 @@ export default class RecipeWrapper {
         });
     }
 
+    /**
+     * Get the URL that should be opened for third party authentication
+     *
+     * @param providerId The identifier for the third party provider. The value must match one of the providers configured with the backend SDK
+     *
+     * @param authorisationURL The URL that should be used for redirection after the third party flow finishes. This is ignored if the backend has a pre-configured redirect_url
+     *
+     * @param providerClientId (OPTIONAL) Client id to be used for the third party provider
+     *
+     * @param userContext (OPTIONAL) Refer to {@link https://supertokens.com/docs/thirdparty/advanced-customizations/user-context the documentation}
+     *
+     * @param options (OPTIONAL) Use this to configure additional properties (for example pre api hooks)
+     *
+     * @returns URL string
+     *
+     * @throws STGeneralError if the API exposed by the backend SDKs returns `status: "GENERAL_ERROR"`
+     */
     static getAuthorisationURLWithQueryParamsAndSetState(input: {
         providerId: string;
         authorisationURL: string;
@@ -50,6 +67,19 @@ export default class RecipeWrapper {
         });
     }
 
+    /**
+     * Sign up/Sign in the user, this method uses the login attempt information from storage
+     *
+     * @param userContext (OPTIONAL) Refer to {@link https://supertokens.com/docs/thirdparty/advanced-customizations/user-context the documentation}
+     *
+     * @param options (OPTIONAL) Use this to configure additional properties (for example pre api hooks)
+     *
+     * @returns `{status: OK, user, createdNewUser: boolean}` if succesful
+     *
+     * @returns `{status: "NO_EMAIL_GIVEN_BY_PROVIDER"}` if the correct scopes are not configured for the third party provider
+     *
+     * @throws STGeneralError if the API exposed by the backend SDKs returns `status: "GENERAL_ERROR"`
+     */
     static signInAndUp(input?: { userContext?: any; options?: RecipeFunctionOptions }): Promise<
         | {
               status: "OK";
