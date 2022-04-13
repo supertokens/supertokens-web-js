4a82968ddd33792f72a0f385a855c8de34a18219
Nemi Shah <nemishah1212@gmail.com>
2022-04-13 14:42:48 +0530

    Remove comment

--- recipe/thirdparty/types/index.d.ts
+++ recipe/thirdparty/types/index.d.ts
@@ -13,12 +13,5 @@
  * under the License.
  */
 export * from "../../../lib/build/recipe/thirdparty/types";
-/**
- * 'export *' does not re-export a default.
- * import NextJS from "supertokens-node/nextjs";
- * the above import statement won't be possible unless either
- * - user add "esModuleInterop": true in their tsconfig.json file
- * - we do the following change:
- */
 import * as _default from "../../../lib/build/recipe/thirdparty/types";
 export default _default;
