b265af728235f1eea472f4ad58801fd5feb563e5
Nemi Shah <nemishah1212@gmail.com>
2022-04-13 10:59:23 +0530

    Add code docs for thidpartyemailpassword functions in recipe index files

--- lib/build/recipe/thirdpartyemailpassword/index.d.ts
+++ lib/build/recipe/thirdpartyemailpassword/index.d.ts
@@ -5,12 +5,28 @@ export default class RecipeWrapper {
         config?: UserInput
     ): import("../../types").CreateRecipeFunction<import("./types").PreAndPostAPIHookAction>;
     static signOut(input?: { userContext?: any }): Promise<void>;
+    /**
+     * Submit a new password for the user
+     *
+     * @param formFields List of fields to send to the API exposed by the backend SDK (Refer to the {@link https://supertokens.com/docs/fdi API spec} to know more)
+     *
+     * @param userContext (OPTIONAL) Refer to {@link https://supertokens.com/docs/thirdpartyemailpassword/advanced-customizations/user-context the documentation}
+     *
+     * @param options (OPTIONAL) Use this to configure additional properties (for example pre api hooks)
+     *
+     * @returns `{status: "OK"}` if successfull
+     *
+     * @returns `{status: "RESET_PASSWORD_INVALID_TOKEN_ERROR"}` if the token in the URL is invalid
+     *
+     * @returns `{status: "FIELD_ERROR", formFields}` if the form field values are incorrect
+     *
+     * @throws STGeneralError if the API exposed by the backend SDKs returns `status: "GENERAL_ERROR"`
+     */
     static submitNewPassword(input: {
         formFields: {
             id: string;
             value: string;
         }[];
-        token?: string;
         options?: RecipeFunctionOptions;
         userContext?: any;
     }): Promise<
@@ -27,6 +43,21 @@ export default class RecipeWrapper {
               fetchResponse: Response;
           }
     >;
+    /**
+     * Send an email to the user for password reset
+     *
+     * @param formFields List of fields to send to the API exposed by the backend SDK (Refer to the {@link https://supertokens.com/docs/fdi API spec} to know more)
+     *
+     * @param userContext (OPTIONAL) Refer to {@link https://supertokens.com/docs/thirdpartyemailpassword/advanced-customizations/user-context the documentation}
+     *
+     * @param options (OPTIONAL) Use this to configure additional properties (for example pre api hooks)
+     *
+     * @returns `{status: "OK"}` if successfull
+     *
+     * @returns `{status: "FIELD_ERROR", formFields}` if the formFields dont match the ones in the configured in the backend SDKs
+     *
+     * @throws STGeneralError if the API exposed by the backend SDKs returns `status: "GENERAL_ERROR"`
+     */
     static sendPasswordResetEmail(input: {
         formFields: {
             id: string;
@@ -48,11 +79,39 @@ export default class RecipeWrapper {
               fetchResponse: Response;
           }
     >;
+    /**
+     * Check if an email exists
+     *
+     * @param email The email to check
+     *
+     * @param userContext (OPTIONAL) Refer to {@link https://supertokens.com/docs/thirdpartyemailpassword/advanced-customizations/user-context the documentation}
+     *
+     * @param options (OPTIONAL) Use this to configure additional properties (for example pre api hooks)
+     *
+     * @returns `{status: "OK", doesExist: boolean}`
+     *
+     * @throws STGeneralError if the API exposed by the backend SDKs returns `status: "GENERAL_ERROR"`
+     */
     static doesEmailExist(input: { email: string; options?: RecipeFunctionOptions; userContext?: any }): Promise<{
         status: "OK";
         doesExist: boolean;
         fetchResponse: Response;
     }>;
+    /**
+     * Sign up a user with email and password
+     *
+     * @param formFields List of fields to send to the API exposed by the backend SDK (Refer to the {@link https://supertokens.com/docs/fdi API spec} to know more). Note that the form fields must match the ones configured in the backend SDKs
+     *
+     * @param userContext (OPTIONAL) Refer to {@link https://supertokens.com/docs/thirdpartyemailpassword/advanced-customizations/user-context the documentation}
+     *
+     * @param options (OPTIONAL) Use this to configure additional properties (for example pre api hooks)
+     *
+     * @returns `{status: "OK"}` if successfull
+     *
+     * @returns `{status: "FIELD_ERROR", formFields}` if the formFields dont match the ones in the configured in the backend SDKs
+     *
+     * @throws STGeneralError if the API exposed by the backend SDKs returns `status: "GENERAL_ERROR"`
+     */
     static emailPasswordSignUp(input: {
         formFields: {
             id: string;
@@ -75,6 +134,23 @@ export default class RecipeWrapper {
               fetchResponse: Response;
           }
     >;
+    /**
+     * Sign in a user with email and password
+     *
+     * @param formFields List of fields to send to the API exposed by the backend SDK (Refer to the {@link https://supertokens.com/docs/fdi API spec} to know more). Note that the form fields must match the ones configured in the backend SDKs
+     *
+     * @param userContext (OPTIONAL) Refer to {@link https://supertokens.com/docs/thirdpartyemailpassword/advanced-customizations/user-context the documentation}
+     *
+     * @param options (OPTIONAL) Use this to configure additional properties (for example pre api hooks)
+     *
+     * @returns `{status: "OK"}` if successfull
+     *
+     * @returns `{status: "FIELD_ERROR", formFields}` if the formFields dont match the ones in the configured in the backend SDKs
+     *
+     * @returns `{status: "WRONG_CREDENTIALS_ERROR"}` if the credentials are invalid
+     *
+     * @throws STGeneralError if the API exposed by the backend SDKs returns `status: "GENERAL_ERROR"`
+     */
     static emailPasswordSignIn(input: {
         formFields: {
             id: string;
@@ -101,6 +177,17 @@ export default class RecipeWrapper {
               fetchResponse: Response;
           }
     >;
+    /**
+     * Sign up/Sign in the user, this method uses the login attempt information from storage
+     *
+     * @param userContext (OPTIONAL) Refer to {@link https://supertokens.com/docs/thirdpartyemailpassword/advanced-customizations/user-context the documentation}
+     *
+     * @param options (OPTIONAL) Use this to configure additional properties (for example pre api hooks)
+     *
+     * @returns `{status: OK, user, createdNewUser: boolean}` if succesful
+     *
+     * @returns `{status: "NO_EMAIL_GIVEN_BY_PROVIDER"}` if the correct scopes are not configured for the third party provider
+     */
     static thirdPartySignInAndUp(input?: { userContext?: any; options?: RecipeFunctionOptions }): Promise<
         | {
               status: "OK";
@@ -113,6 +200,21 @@ export default class RecipeWrapper {
               fetchResponse: Response;
           }
     >;
+    /**
+     * Get the URL that should be opened for third party authentication
+     *
+     * @param providerId The identifier for the third party provider. The value must match one of the providers configured with the backend SDK
+     *
+     * @param authorisationURL The URL that should be used for redirection after the third party flow finishes. This is ignored if the backend has a pre-configured redirect_url
+     *
+     * @param providerClientId (OPTIONAL) Client id to be used for the third party provider
+     *
+     * @param userContext (OPTIONAL) Refer to {@link https://supertokens.com/docs/thirdpartyemailpassword/advanced-customizations/user-context the documentation}
+     *
+     * @param options (OPTIONAL) Use this to configure additional properties (for example pre api hooks)
+     *
+     * @returns URL string
+     */
     static getAuthorisationURLWithQueryParamsAndSetState(input: {
         providerId: string;
         authorisationURL: string;
