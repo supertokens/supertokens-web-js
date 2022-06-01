e8e7a6a891b3dbd9a55af6b031068f390a25f84f
Nemi Shah <nemishah1212@gmail.com>
2022-06-01 11:54:25 +0530

    Rename pre commit file

--- hooks/pre-commit.sh
+++ hooks/pre-commit.sh
@@ -21,7 +21,7 @@ then
    git stash push -k -u -- $files_to_stash >/dev/null 2>/dev/null
 fi
 
-npm run build-check
+npm run build-check >/dev/null 2>/dev/null
 compiles=$?
 
 echo "$(tput setaf 3)* Compiles?$(tput sgr 0)"
