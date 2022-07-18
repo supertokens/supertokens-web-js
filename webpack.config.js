709ab585b9858a17649d8ec27b72465405979891
Nemi Shah <nemishah1212@gmail.com>
2022-07-18 18:29:08 +0530

    Update package version and CHANGELOG

--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,7 @@ var config = {
         supertokensEmailVerification: {
             import: APP_DIR + "/lib/build/recipe/emailverification/index.js",
             filename: "emailverification.js",
+            dependOn: "supertokensWebsite",
         },
         supertokensEmailPassword: {
             import: APP_DIR + "/lib/build/recipe/emailpassword/index.js",
@@ -36,19 +37,31 @@ var config = {
         supertokensThirdParty: {
             import: APP_DIR + "/lib/build/recipe/thirdparty/index.js",
             filename: "thirdparty.js",
+            dependOn: "supertokensWebsite",
         },
         supertokensThirdPartyEmailPassword: {
             import: APP_DIR + "/lib/build/recipe/thirdpartyemailpassword/index.js",
             filename: "thirdpartyemailpassword.js",
+            dependOn: "supertokensWebsite",
         },
         supertokensPasswordless: {
             import: APP_DIR + "/lib/build/recipe/passwordless/index.js",
             filename: "passwordless.js",
+            dependOn: "supertokensWebsite",
         },
         supertokensThirdPartyPasswordless: {
             import: APP_DIR + "/lib/build/recipe/thirdpartypasswordless/index.js",
             filename: "thirdpartypasswordless.js",
+            dependOn: "supertokensWebsite",
         },
+        /**
+         * Without this webpack will bundle supertokens-website as independent references
+         * within each recipe bundle. Any shared resources (WindowHandler for example) will no
+         * longer be initialised in this case
+         *
+         * By separating this and making the recipes depend on this new bundle we ensure that all
+         * usages of the supertokens-website SDK use the same reference.
+         */
         supertokensWebsite: {
             import: "supertokens-website",
             filename: "website.js",
