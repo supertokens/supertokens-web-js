e3bf5bb574807212278de2702b54f9ffb723ad34
Alisher <alisher@supertokens.com>
2023-01-31 15:03:22 +0600

    re-export getGlobalClaimValidators from website

--- lib/build/utils.d.ts
+++ lib/build/utils.d.ts
@@ -1,4 +1,5 @@
 import { AppInfoUserInput, NormalisedAppInfo } from "./types";
+import { SessionClaimValidator } from "supertokens-website";
 export declare function appendQueryParamsToURL(stringUrl: string, queryParams?: Record<string, string>): string;
 export declare function normaliseInputAppInfoOrThrowError(appInfo: AppInfoUserInput): NormalisedAppInfo;
 export declare function isTest(): boolean;
@@ -6,3 +7,10 @@ export declare function getQueryParams(param: string): string | undefined;
 export declare function checkForSSRErrorAndAppendIfNeeded(error: string): string;
 export declare function getNormalisedUserContext(userContext?: any): any;
 export declare function getHashFromLocation(): string;
+export declare function getGlobalClaimValidators(
+    overrideGlobalClaimValidators?: (
+        globalClaimValidators: SessionClaimValidator[],
+        userContext: any
+    ) => SessionClaimValidator[],
+    userContext?: any
+): SessionClaimValidator[];
