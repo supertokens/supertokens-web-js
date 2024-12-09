16a06805b633e15b4995aa2734315f60bc5f4992
Deepjyoti Barman <deepjyoti@supertokens.com>
2024-12-09 15:26:52 +0530

    Add recipe definition for webauthn

--- lib/ts/recipe/webauthn/recipe.ts
+++ lib/ts/recipe/webauthn/recipe.ts
@@ -14,5 +14,65 @@
  */
 
 import AuthRecipe from "../authRecipe";
-import { PreAndPostAPIHookAction } from "../multitenancy";
-import { NormalisedInputType, RecipeInterface, InputType } from "./types";
+import { NormalisedInputType, RecipeInterface, InputType, UserInput, PreAndPostAPIHookAction } from "./types";
+import { normaliseUserInput } from "./utils";
+import OverrideableBuilder from "supertokens-js-override";
+import RecipeImplementation from "./recipeImplementation";
+import { CreateRecipeFunction, NormalisedAppInfo } from "../../types";
+import { checkForSSRErrorAndAppendIfNeeded, isTest } from "../../utils";
+
+export default class Recipe extends AuthRecipe<PreAndPostAPIHookAction, NormalisedInputType> {
+    static instance?: Recipe;
+    static RECIPE_ID = "webauthn";
+
+    recipeImplementation: RecipeInterface;
+
+    constructor(config: InputType) {
+        super(normaliseUserInput(config));
+
+        const builder = new OverrideableBuilder(
+            RecipeImplementation({
+                recipeId: this.config.recipeId,
+                appInfo: this.config.appInfo,
+                clientType: this.config.clientType,
+                preAPIHook: this.config.preAPIHook,
+                postAPIHook: this.config.postAPIHook,
+            })
+        );
+        this.recipeImplementation = builder.override(this.config.override.functions).build();
+    }
+
+    static init(config?: UserInput): CreateRecipeFunction<PreAndPostAPIHookAction> {
+        return (appInfo: NormalisedAppInfo, clientType: string | undefined) => {
+            Recipe.instance = new Recipe({
+                ...config,
+                recipeId: Recipe.RECIPE_ID,
+                appInfo,
+                clientType,
+            });
+
+            return Recipe.instance;
+        };
+    }
+
+    static getInstanceOrThrow(): Recipe {
+        if (Recipe.instance === undefined) {
+            let error = "No instance of Webauthn found. Make sure to call the Webauthn.init method.";
+            error = checkForSSRErrorAndAppendIfNeeded(error);
+
+            throw Error(error);
+        }
+
+        return Recipe.instance;
+    }
+
+    static reset(): void {
+        if (!isTest()) {
+            return;
+        }
+        Recipe.instance = undefined;
+        return;
+    }
+}
+
+export { Recipe };
--- lib/ts/recipe/webauthn/recipeImplementation.ts
+++ lib/ts/recipe/webauthn/recipeImplementation.ts
@@ -335,3 +335,5 @@ export default function getRecipeImplementation(
         },
     };
 }
+
+export { getRecipeImplementation };
